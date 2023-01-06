151b6c552f15358df088998328afefa45c8a1663
Vipul Mehta <tovipul21@gmail.com>
2023-01-06 09:56:31 +0530

    * Added new library for common reusable code * Added new test file for testing the Common code library

---
+++ Acme.Common/StringHandler.cs
@@ -0,0 +1,40 @@
+﻿namespace Acme.Common
+{
+    public class StringHandler
+    {
+        public string InsertSpaces(string source)
+        {
+            string result = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(source))
+            {
+                // Solution 1
+                //char[] arr = source.ToCharArray();
+
+                //for (int i = 0; i < arr.Length; i++)
+                //{
+                //    if (char.IsUpper(arr[i]) && i > 0)
+                //    {
+                //        result += " ";
+                //    }
+
+                //    result += arr[i];
+                //}
+
+                // Solution 2
+                foreach (char letter in source)
+                {
+                    if (char.IsUpper(letter))
+                    {
+                        result = result.Trim();
+                        result += " ";
+                    }
+
+                    result += letter;
+                }
+            }
+
+            return result.Trim();
+        }
+    }
+}
---
+++ Tests/ACM.BL.Test/CommonTest.cs
@@ -0,0 +1,40 @@
+﻿using Acme.Common;
+
+namespace ACM.BL.Test
+{
+    public class CommonTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void InsertSpaceTest()
+        {
+            // Arrange
+            StringHandler sh = new();
+            string expected = "Vipul Mehta";
+
+            // Act
+            string actual = sh.InsertSpaces("VipulMehta");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InsertTextWithSpaceTest()
+        {
+            // Arrange
+            StringHandler sh = new();
+            string expected = "Vipul k Mehta";
+
+            // Act
+            string actual = sh.InsertSpaces("Vipul k Mehta");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
